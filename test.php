01f6247362a23849c80c9269126a854ee6426ed2
Sergey <cool_swat@mail.ru>
2018-12-29 01:40:55 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,8 @@
 <?php 
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 if (isset($_GET["test"])) {
 $data = file_get_contents(__DIR__ .DIRECTORY_SEPARATOR.$_GET["test"]);
 $test = json_decode($data, true);
@@ -41,6 +45,7 @@ if(!empty($_POST)) {
  	<p>
 	<?php if(isset($correct)) {echo "Правильных ответов: ".$correct;}?>
  	</p>
+ 	<p><a href="list.php">К списку тестов</a></p>
  </body>
  </html>
 
